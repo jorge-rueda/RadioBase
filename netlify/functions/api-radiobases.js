047dc553394126b00a771046c016ee20bc6216c2
Jorge Rueda <alberto1443@hotmail.com>
2024-07-30 15:05:48 -0600

    arreglo

--- netlify/functions/api-radiobases.js
+++ netlify/functions/api-radiobases.js
@@ -1,10 +1,8 @@
 const { MongoClient } = require('mongodb');
 
-// Usa las variables de entorno configuradas
-const uri = process.env.MONGO_URI; 
-const dbName = process.env.DB_NAME; // Si es necesario
-const dbUser = process.env.DB_USER; // Si es necesario
-const dbPassword = process.env.DB_PASSWORD; // Si es necesario
+const uri = process.env.MONGO_URI;
+const dbName = process.env.DB_NAME || 'radiobase'; // Default to 'radiobase' if not provided
+const collectionName = 'radiobase'; // Default collection name
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -15,8 +13,8 @@ async function connectToDatabase() {
   if (!db) {
     try {
       await client.connect();
-      db = client.db(dbName || 'radiobase'); // Usa DB_NAME si está disponible
-      collection = db.collection('radiobase'); // O usa una variable si la colección puede cambiar
+      db = client.db(dbName);
+      collection = db.collection(collectionName);
     } catch (error) {
       console.error('Failed to connect to database:', error);
       throw new Error('Database connection error');
